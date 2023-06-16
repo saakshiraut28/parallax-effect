f4e7e96e3bd8b3ed97bcaad03e68e3324ea65362
saakshiraut28 <saakshiraut28@gmail.com>
2023-06-16 22:47:37 +0530

    Project Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
+import logo from "./logo.svg";
+import "./App.css";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import Img1 from "./images/roof.jpg";
+import Img2 from "./images/vlg.jpg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <Parallax pages={3} className="bg-indigo-200">
+        <ParallaxLayer offset={0} speed={2.5}>
+          <div className="flex w-full h-full justify-center items-center bg-red-300">
+            {" "}
+            <span className="text-8xl font-black">Hello World !!!!</span>
+          </div>
+          <img src={Img1} />
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={1}>
+          <div className="flex w-full justify-center items-center bg-red-300">
+            {" "}
+            <span className="text-8xl font-black">
+              Did you notice some changes while scrolling?
+            </span>
+          </div>
+          <img src={Img2} />
+        </ParallaxLayer>
+        <ParallaxLayer offset={2} speed={2.5}>
+          <div className="flex w-full h-full justify-center items-center bg-red-300">
+            {" "}
+            <span className="text-8xl font-black">Thanks For Visiting 😎</span>
+          </div>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
